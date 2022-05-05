19e3af6bb625417ba70737b6ae27524f622d061f
KenwoodFox <kenwood364@gmail.com>
2022-05-04 21:09:45 -0400

    Cleanup

--- Software/pubsub/pubsub/subscriber_member_function.py
+++ Software/pubsub/pubsub/subscriber_member_function.py
@@ -19,14 +19,11 @@ from std_msgs.msg import String
 
 
 class MinimalSubscriber(Node):
-
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__("minimal_subscriber")
         self.subscription = self.create_subscription(
-            String,
-            'topic',
-            self.listener_callback,
-            10)
+            String, "topic", self.listener_callback, 10
+        )
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
@@ -47,5 +44,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
