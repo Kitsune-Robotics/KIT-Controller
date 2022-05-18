65b8cb33cab0d9ad47e6c486b05a3cb4ec29f6fb
KenwoodFox <kenwood364@gmail.com>
2022-05-18 05:47:20 +0000

    Halfway to a new package

---
+++ Software/src/i2c_lcdsub/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'i2c_lcdsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools','smbus','rpi-lcd'],
+    zip_safe=True,
+    maintainer='joe',
+    maintainer_email='joe@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
