6e3284566eca5e0c67495fcf51f2918e4b79d38c
KenwoodFox <kenwood364@gmail.com>
2022-06-09 13:47:39 -0400

    Bump

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,17 +1,21 @@
-// Arduino
-#include "Arduino.h"
+#include <Arduino.h>
 
-void setup()
+// the setup routine runs once when you press reset:
+void setup(void)
 {
-    // initialize digital pin LED_BUILTIN as an output.
-    pinMode(25, OUTPUT);
+    // initialize the digital pin as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
+// the loop routine runs over and over again forever:
 void loop()
 {
-    digitalWrite(25, HIGH); // turn the LED on (HIGH is the voltage level)
-    delay(1000);            // wait for a second
-    digitalWrite(25, LOW);  // turn the LED off by making the voltage LOW
-    delay(1000);            // wait for a second
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    Serial.print("High");
+    delay(1000);                    // wait for a second
+    digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+    Serial.print("Low");
+    delay(1000); // wait for a second
 }
