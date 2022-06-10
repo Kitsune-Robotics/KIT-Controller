ecf9d8b14efba1042f08cac9ff6003364ffc658d
KenwoodFox <kenwood364@gmail.com>
2022-06-10 08:37:21 -0400

    Fix serial debugging issue, thanks to Jck_true on IRC

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -5,17 +5,15 @@ void setup(void)
 {
     // initialize the digital pin as an output.
     pinMode(LED_BUILTIN, OUTPUT);
-
-    Serial.begin(115200);
 }
 
 // the loop routine runs over and over again forever:
 void loop()
 {
     digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-    Serial.print("High");
+    Serial.println("High");
     delay(1000);                    // wait for a second
     digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
-    Serial.print("Low");
+    Serial.println("Low");
     delay(1000); // wait for a second
 }
