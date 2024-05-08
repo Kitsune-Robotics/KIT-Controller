b90f9cfa38643f8488d6f0bb48c884518ce1844c
KenwoodFox <kenwood364@gmail.com>
2024-05-08 13:16:11 -0400

    My first build

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
 void led_task()
-{   
+{
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
+    while (true)
+    {
         gpio_put(LED_PIN, 1);
         vTaskDelay(100);
         gpio_put(LED_PIN, 0);
@@ -24,5 +24,7 @@ int main()
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
-    while(1){};
+    while (1)
+    {
+    };
 }
