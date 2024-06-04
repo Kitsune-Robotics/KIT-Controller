b0b273aea28055a8f17955066f356263f78fd645
KenwoodFox <kenwood364@gmail.com>
2024-06-04 11:52:24 -0400

    Early prototype for usb_console, works pretty well :

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,7 +1,25 @@
-#include "FreeRTOS.h"
-#include "task.h"
+/* Raspberry Pi Libs */
 #include <stdio.h>
 #include "pico/stdlib.h"
+
+/* FreeRTOS */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+// This wont stay here
+#define MAX_COMMAND_LENGTH 256
+#define MAX_ARGS 10
+#define MAX_ARG_LENGTH 32
+typedef struct
+{
+    char command[MAX_COMMAND_LENGTH]; // Full command string
+    char *argv[MAX_ARGS];             // Pointers to arguments
+    int argc;                         // Argument count
+} Command_t;
+
+// Queues
+static QueueHandle_t cmdQueue = NULL;
 
 void led_task(void *pvParams)
 {
@@ -17,13 +35,37 @@ void led_task(void *pvParams)
     }
 }
 
+void usb_console(void *pvParams)
+{
+    // Setup variables we'll need
+
+    while (true)
+    {
+        // Read and echo each character
+
+        // check if the line is complete
+        // if it is, add a formatted Command_t to the cmdQueue
+
+        printf("Hello world!\n");
+
+        // Short delay
+        vTaskDelay(100);
+    }
+}
+
 int main()
 {
     // From pico SDK, init all
     stdio_init_all();
 
+    // FreeRTOS Create queues
+    cmdQueue = xQueueCreate(2, sizeof(Command_t *));
+
     // FreeRTOS Create tasks
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(usb_console, "USB_Console", 256, NULL, 1, NULL);
+
+    // Begin scheduler
     vTaskStartScheduler();
 
     while (true)
